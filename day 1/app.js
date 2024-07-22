a9862d803aa16985e3bbd82ad65254dd1adac219
Hasankhan170 <hasankhanali0316@gmail.com>
2024-07-22 09:48:17 -0700

    task 3

--- day 1/app.js
+++ day 1/app.js
@@ -12,5 +12,22 @@
 // constant declaration 
 // task 2
 
-const boolean = true;
-console.log(boolean);
+// const boolean = true;
+// console.log(boolean);
+
+
+// data types 
+// task 3
+
+// let num = 23
+// let str = 'hasan'
+// let boolean = false
+// let arr =[]
+// let obj = {}
+
+// console.log(typeof num); 
+// console.log(typeof str)
+// console.log(typeof boolean); 
+// console.log(typeof arr); 
+// console.log(typeof obj); 
+
