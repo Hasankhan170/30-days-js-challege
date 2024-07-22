684f6a8b5b59ab4e907773cc0a61b0d332213cf0
Hasankhan170 <hasankhanali0316@gmail.com>
2024-07-22 09:40:41 -0700

    task 2

---
+++ day 1/app.js
@@ -0,0 +1,16 @@
+//Day 1 variables and datatypes
+
+// variable declaration 
+// task 1
+
+// var num = 23
+// console.log(num);
+
+// let str = 'my name is hasan'
+// console.log(str);
+
+// constant declaration 
+// task 2
+
+const boolean = true;
+console.log(boolean);
--- task 1/app.js
+++
@@ -1,6 +0,0 @@
-// variables and datatypes
-
-// variable declaration 
-
-var user = 23
-console.log(user);
