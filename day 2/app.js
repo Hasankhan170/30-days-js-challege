f972aced4f60ea9053392fd2849123ae6b4b2e3d
Hasankhan170 <hasankhanali0316@gmail.com>
2024-07-22 23:46:17 -0700

    complete activity 1

---
+++ day 2/app.js
@@ -0,0 +1,37 @@
+// DAY 2 OPERATORS
+
+// Activity 1 : Arithmetic operations 
+
+// task 1
+
+// let x = 10;
+// let y = 5;
+// console.log(x + y);
+
+
+// task 2
+
+// let x = 10;
+// let y = 5;
+// console.log(x - y);
+
+
+// task 3
+
+// let x = 10;
+// let y = 5;
+// console.log(x * y);
+
+
+// task 4
+
+// let x = 10;
+// let y = 5;
+// console.log(x / y);
+
+
+// task 5
+
+// let x = 10;
+// let y = 50;
+// console.log(x % y);
